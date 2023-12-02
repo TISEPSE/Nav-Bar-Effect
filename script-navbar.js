b069620629e8e0de0949360ded2c0ca0aaf7eea1
TISEPSE <143852263+TISEPSE@users.noreply.github.com>
2023-12-02 10:54:37 +0100

    Update script-navbar.js

--- script-navbar.js
+++ script-navbar.js
@@ -59,4 +59,11 @@ const span = document.querySelector('.bck-grnd');
          liste.classList.toggle('active')
      }
 
-     
+     document.addEventListener('DOMContentLoaded', function(){
+        let cursor =document.querySelector (".cursor")
+        
+        document.addEventListener('mousemove' , function(e) {
+            cursor.style.left = e.clientX+ 'px'
+            cursor.style.top = e.clientY+ 'px'
+        })
+     })
